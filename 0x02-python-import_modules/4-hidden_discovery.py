21e13113cb52749bfa357cb64935799a094aa10f
shaffy95 <onozareshaffy95@example.com>
2023-12-06 05:42:15 -0800

    import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    value = dir(hidden_4)
-    for value in values:
-        if value[:2] != "__":
-            print(value)
+    values = dir(hidden_4)
+    for name in values:
+        if name[:2] != "__":
+            print(name)
