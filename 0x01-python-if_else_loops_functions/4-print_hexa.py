278fc334436e5a2e256ecb6fe38ea4ac3d480b95
shaffy95 <onozareshaffy95@example.com>
2023-12-06 03:56:26 -0800

    Else/if_Python

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for value in range(0, 99):
+    print("{} = {}".format(value, hex(value)))
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for value in range(00, 100):
+    if value <= 99:
+        print("{:02}".format(value), end='\n')
+    else:
+        print("{:02}".format(value), end=', ')
