d2f1765df7a16eb169a027d7f60ec183dd12138f
shaffy95 <onozareshaffy95@example.com>
2023-12-06 03:50:51 -0800

    Else/if_Python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for value in range(97, 123):
-    if chr(value) != '99' and chr(value) != '120':
+    if chr(value) != 'q' and chr(value) != 'e':
         print('{:c}'.format(value), end='')
