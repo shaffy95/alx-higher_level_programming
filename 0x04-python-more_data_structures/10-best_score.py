ee3775eea934817153f83f5b9c6d2ef31fabe1a2
shaffy95 <onozareshaffy95@example.com>
2023-12-07 07:55:26 -0800

    Python_data/structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max_score = 0
     min_score = 0
-    if a_dictionary is not a_dictionary:
+    if a_dictionary is None or not a_dictionary:
         return None
     else:
         best_key = max(a_dictionary, key=a_dictionary.get)
